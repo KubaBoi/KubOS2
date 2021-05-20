7d162e85f5a226383967be706e3d921df2baaca1
KubaBoi <jakubanderle@outlook.cz>
2021-05-20 19:58:39 +0200

    mouse

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -90,10 +90,10 @@ public:
     
     virtual void OnMouseMove(int xoffset, int yoffset)
     {
-        /*static uint16_t* VideoMemory = (uint16_t*)0xb8000;
+        static uint16_t* VideoMemory = (uint16_t*)0xb8000;
         VideoMemory[80*y+x] = (VideoMemory[80*y+x] & 0x0F00) << 4
                             | (VideoMemory[80*y+x] & 0xF000) >> 4
-                            | (VideoMemory[80*y+x] & 0x00FF);*/
+                            | (VideoMemory[80*y+x] & 0x00FF);
 
         x += xoffset;
         if(x >= 80) x = 79;
@@ -102,9 +102,9 @@ public:
         if(y >= 25) y = 24;
         if(y < 0) y = 0;
 
-        /*VideoMemory[80*y+x] = (VideoMemory[80*y+x] & 0x0F00) << 4
+        VideoMemory[80*y+x] = (VideoMemory[80*y+x] & 0x0F00) << 4
                             | (VideoMemory[80*y+x] & 0xF000) >> 4
-                            | (VideoMemory[80*y+x] & 0x00FF);*/
+                            | (VideoMemory[80*y+x] & 0x00FF);
     }
     
 };
