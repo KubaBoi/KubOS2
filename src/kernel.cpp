6a2c5a6fcb54d93c5e37b00a96700f4f4c2d19b2
KubaBoi <jakubanderle@outlook.cz>
2021-05-21 01:01:03 +0200

    bluescreen

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -6,6 +6,7 @@
 #include <drivers/driver.h>
 #include <drivers/keyboard.h>
 #include <drivers/mouse.h>
+#include <drivers/vga.h>
 
 using namespace kubos;
 using namespace kubos::common;
@@ -157,11 +158,20 @@ extern "C" void kernelMain(const void* multiboot_structure, uint32_t /*multiboot
     PeripheralComponentInterconnectController PCIController;
     PCIController.SelectDrivers(&drvManager, &interrupts);
 
+    VideoGraphicsArray vga;
+
     printf("Initializing Hardware, Stage 2\n");
     drvManager.ActivateAll();
         
     printf("Initializing Hardware, Stage 3\n");
     interrupts.Activate();
+
+    vga.SetMode(320,200,8);
+    for (uint32_t y = 0; y < 200; y++) {
+        for (uint32_t x = 0; x < 320; x++) {
+            vga.PutPixel(x, y, 0x00, 0x00, 0xA8);
+        }
+    }
 
     while(1);
 }
